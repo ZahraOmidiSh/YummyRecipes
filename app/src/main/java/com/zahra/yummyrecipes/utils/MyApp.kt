81f1cf518ac25c89b2d3db2726a54bd58e319c29
Zahra Omidi <Zahram@gm.com>
2023-07-19 02:02:34 +0330

    add fonts to assets

---
+++ app/src/main/java/com/zahra/yummyrecipes/utils/MyApp.kt
@@ -0,0 +1,24 @@
+package com.zahra.yummyrecipes.utils
+
+import android.app.Application
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        //Calligraphy
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath("fonts/Roboto-RobotoRegular.ttf")
+                            .build()
+                    )
+                )
+                .build()
+        )
+    }
+}
