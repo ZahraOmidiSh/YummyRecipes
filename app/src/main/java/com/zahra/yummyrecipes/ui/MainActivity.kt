86de5381dd6a5cc682e094a1a33dd8ddd1b06d23
Zahra Omidi <Zahram@gm.com>
2023-07-19 02:15:08 +0330

    add binding

--- app/src/main/java/com/zahra/yummyrecipes/ui/MainActivity.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/MainActivity.kt
@@ -2,11 +2,27 @@ package com.zahra.yummyrecipes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.zahra.yummyrecipes.R
+import com.zahra.yummyrecipes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Binding
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    //Other
+    private lateinit var navHost : NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
 }
+
