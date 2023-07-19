034299ea334cb778c5d7a300732a425d6e0b77f6
Zahra Omidi <Zahram@gm.com>
2023-07-19 19:16:47 +0330

    update CheckConnection

--- app/src/main/java/com/zahra/yummyrecipes/utils/NetworkChecker.kt
+++ app/src/main/java/com/zahra/yummyrecipes/utils/NetworkChecker.kt
@@ -1,4 +1,19 @@
 package com.zahra.yummyrecipes.utils
 
-class NetworkChecker {
+import android.net.ConnectivityManager
+import android.net.NetworkRequest
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+class NetworkChecker @Inject constructor(
+    private val manager: ConnectivityManager,
+    private val request: NetworkRequest
+) : ConnectivityManager.NetworkCallback() {
+
+    private val isNetworkAvailable = MutableStateFlow(false)
+
+    fun checkNetworkAvailability(): MutableStateFlow<Boolean> {
+        //Register
+        manager.registerNetworkCallback(request, this)
+    }
 }
