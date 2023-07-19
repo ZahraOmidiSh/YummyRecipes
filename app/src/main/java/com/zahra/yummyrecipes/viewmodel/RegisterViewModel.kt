8a24dd7708326608c92fbdd00f75fe8e0a4c76d6
Zahra Omidi <Zahram@gm.com>
2023-07-19 23:39:20 +0330

    update RegisterViewModel

---
+++ app/src/main/java/com/zahra/yummyrecipes/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,24 @@
+package com.zahra.yummyrecipes.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.zahra.yummyrecipes.data.repository.RegisterRepository
+import com.zahra.yummyrecipes.models.register.BodyRegister
+import com.zahra.yummyrecipes.models.register.ResponseRegister
+import com.zahra.yummyrecipes.utils.NetworkRequest
+import com.zahra.yummyrecipes.utils.NetworkResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(private val repository: RegisterRepository):ViewModel(){
+    //Api
+    val registerData = MutableLiveData<NetworkRequest<ResponseRegister>>()
+    fun  callRegisterApi(apiKey:String , body: BodyRegister) = viewModelScope.launch {
+        registerData.value=NetworkRequest.Loading()
+        val response = repository.postRegister(apiKey, body)
+        registerData.value=NetworkResponse(response).generalNetworkResponse()
+    }
+}
