ecf1a670b34452b29ffb3cfe5df85a079a990993
Zahra Omidi <Zahram@gm.com>
2023-07-20 03:11:06 +0330

    update RegisterFragment

--- app/src/main/java/com/zahra/yummyrecipes/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/register/RegisterFragment.kt
@@ -1,21 +1,44 @@
 package com.zahra.yummyrecipes.ui.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.zahra.yummyrecipes.R
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.withStarted
 import com.zahra.yummyrecipes.databinding.FragmentRegisterBinding
+import com.zahra.yummyrecipes.models.register.BodyRegister
+import com.zahra.yummyrecipes.utils.NetworkChecker
+import com.zahra.yummyrecipes.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RegisterFragment : Fragment() {
     //Binding
-    private var _binding:FragmentRegisterBinding?=null
+    private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    @Inject
+    lateinit var body:BodyRegister
+
+    @Inject
+    lateinit var networkChecker: NetworkChecker
+
+    //Other
+    private val viewModel: RegisterViewModel by viewModels()
+    private var email = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
@@ -23,13 +46,46 @@ class RegisterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         //Init views
         binding.apply {
+            eMailEdt.addTextChangedListener {
+                eMailEdt.error = null
+            }
+                //Register Btn Click
+                registerButton.setOnClickListener {
+                    val firstname = firstNameEdt.text.toString()
+                    val lastname = lastNameEdt.text.toString()
+                    val username = userNameEdt.text.toString()
+                    val email = eMailEdt.text.toString()
+                    if (firstname.isEmpty() || lastname.isEmpty() || username.isEmpty() || email.isEmpty()) {
+                        Toast.makeText(requireContext(), "Please fill all the boxes!!", Toast.LENGTH_SHORT).show()
+                    }else{
+                        if(!email.contains("@") || !email.contains(".com")){
+                            eMailEdt.error="Email is not valid"
+                        }else{
+                            //Body
+                            body.email=email
+                            body.firstName=firstname
+                            body.lastName=lastname
+                            body.username=username
+                            //CheckNetwork
+//                            lifecycleScope.launch {
+//                                withStarted {
+//                                    networkChecker.checkNetworkAvailability().collect {state ->
+//                                        if(state){
+//
+//                                }
+//
+//                            }
 
+                        }
+                    }
+                }
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        _binding=null
+        _binding = null
     }
 
 }
--- app/src/main/java/com/zahra/yummyrecipes/utils/MyApp.kt
+++ app/src/main/java/com/zahra/yummyrecipes/utils/MyApp.kt
@@ -1,10 +1,12 @@
 package com.zahra.yummyrecipes.utils
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import io.github.inflationx.calligraphy3.CalligraphyConfig
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor
 import io.github.inflationx.viewpump.ViewPump
 
+@HiltAndroidApp
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
