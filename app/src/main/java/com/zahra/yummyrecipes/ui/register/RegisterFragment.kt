31a5f08c194116b19196c21decc6e2a8db904108
Zahra Omidi <Zahram@gm.com>
2023-07-20 03:55:26 +0330

    update RegisterFragment.kt

--- app/src/main/java/com/zahra/yummyrecipes/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/register/RegisterFragment.kt
@@ -10,12 +10,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.withStarted
+import com.zahra.yummyrecipes.R
 import com.zahra.yummyrecipes.databinding.FragmentRegisterBinding
 import com.zahra.yummyrecipes.models.register.BodyRegister
+import com.zahra.yummyrecipes.utils.Constants.MY_API_KEY
 import com.zahra.yummyrecipes.utils.NetworkChecker
+import com.zahra.yummyrecipes.utils.NetworkRequest
+import com.zahra.yummyrecipes.utils.showSnackBar
 import com.zahra.yummyrecipes.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -26,7 +29,7 @@ class RegisterFragment : Fragment() {
     private val binding get() = _binding!!
 
     @Inject
-    lateinit var body:BodyRegister
+    lateinit var body: BodyRegister
 
     @Inject
     lateinit var networkChecker: NetworkChecker
@@ -56,14 +59,10 @@ class RegisterFragment : Fragment() {
                 val username = userNameEdt.text.toString()
                 val email = eMailEdt.text.toString()
                 if (firstname.isEmpty() || lastname.isEmpty() || username.isEmpty() || email.isEmpty()) {
-                    Toast.makeText(
-                        requireContext(),
-                        "Please fill all the boxes!!",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Toast.makeText(requireContext(), "Please fill all the boxes!!", Toast.LENGTH_SHORT).show()
                 } else {
                     if (!email.contains("@") || !email.contains(".com")) {
-                        eMailEdt.error = "Email is not valid"
+                        eMailEdt.error = getString(R.string.email_is_not_valid)
                     } else {
                         //Body
                         body.email = email
@@ -71,22 +70,43 @@ class RegisterFragment : Fragment() {
                         body.lastName = lastname
                         body.username = username
                         //CheckNetwork
-//                            lifecycleScope.launch {
-//                                withStarted {
-//                                    networkChecker.checkNetworkAvailability().collect {state ->
-//                                        if(state){
-//
-//                                }
-//
-//                            }
-//
-//                        }
-//                    }
+                        lifecycleScope.launch {
+                            withStarted {}
+                            networkChecker.checkNetworkAvailability().collect { state ->
+                                if (state) {
+                                    //Call api
+                                    viewModel.callRegisterApi(MY_API_KEY, body)
+
+                                } else {
+                                    root.showSnackBar(getString(R.string.check_internet))
+                                }
+
+                            }
+                        }
                     }
                 }
             }
+            //Load data
+            loadRegisterData()
         }
     }
+
+    private fun loadRegisterData() {
+        viewModel.registerData.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is NetworkRequest.Loading -> {}
+                is NetworkRequest.Success -> {
+
+                }
+
+                is NetworkRequest.Error -> {
+                    binding.root.showSnackBar(response.message!!)
+                }
+            }
+
+        }
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
