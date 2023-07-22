4e5224a7c0011c7a51f9faa5d0319ef453dd9562
Zahra Omidi <Zahram@gm.com>
2023-07-22 18:51:50 +0330

    update RegisterRepository

--- app/src/main/java/com/zahra/yummyrecipes/data/repository/RegisterRepository.kt
+++ app/src/main/java/com/zahra/yummyrecipes/data/repository/RegisterRepository.kt
@@ -4,16 +4,21 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.zahra.yummyrecipes.data.source.RemoteDataSource
 import com.zahra.yummyrecipes.models.register.BodyRegister
+import com.zahra.yummyrecipes.models.register.RegisterStoredModel
 import com.zahra.yummyrecipes.utils.Constants.REGISTER_HASH
-import com.zahra.yummyrecipes.utils.Constants.REGISTER_USERINFO
 import com.zahra.yummyrecipes.utils.Constants.REGISTER_USERNAME
 import com.zahra.yummyrecipes.utils.Constants.REGISTER_USER_INFO
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 import javax.inject.Inject
 
 @ActivityRetainedScoped
@@ -36,6 +41,19 @@ class RegisterRepository @Inject constructor(
             it[StoreKeys.hash]=hash
         }
     }
+
+    val readRegisterData:Flow<RegisterStoredModel> = context.dataStore.data
+        .catch {  e->
+            if(e is IOException){
+                emit(emptyPreferences())
+            }else{
+                throw  e
+            }
+        }.map {
+            val username = it[StoreKeys.username] ?: ""
+            val hash = it[StoreKeys.hash] ?: ""
+            RegisterStoredModel(username , hash)
+        }
 
     // API
     suspend fun postRegister(apiKey: String, body: BodyRegister) = remote.postRegister(apiKey, body)
---
+++ app/src/main/java/com/zahra/yummyrecipes/models/register/RegisterStoredModel.kt
@@ -0,0 +1,3 @@
+package com.zahra.yummyrecipes.models.register
+
+data class RegisterStoredModel(val username:String , val hash:String)
