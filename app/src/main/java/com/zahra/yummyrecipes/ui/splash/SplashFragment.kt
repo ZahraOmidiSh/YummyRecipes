4853791e28eede8c3046a282d14f45171ff5d773
Zahra Omidi <Zahram@gm.com>
2023-07-23 16:48:46 +0330

    update splash fragment

--- app/src/main/java/com/zahra/yummyrecipes/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/splash/SplashFragment.kt
@@ -1,36 +1,59 @@
 package com.zahra.yummyrecipes.ui.splash
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.zahra.yummyrecipes.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.zahra.yummyrecipes.databinding.FragmentSplashBinding
+import com.zahra.yummyrecipes.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
-    private var _binding:FragmentSplashBinding?=null
+    //Binding
+    private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding=FragmentSplashBinding.inflate(layoutInflater,container,false)
+    //Other
+    private val viewModel: RegisterViewModel by viewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(layoutInflater)
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //InitViews
         binding.apply {
-            //Application version
-            versionTxt.text = "${R.string.version} : ${BuildConfig.VERSION_NAME}"
+            //Application Version
+            val manager = context?.packageManager
+            val info = context?.packageName?.let {
+                manager?.getPackageInfo(
+                    it, 0
+                )
+            }
+            val versionName = info?.versionName
+            versionTxt.text = versionName
+            //Auto navigate
+
+
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        _binding=null
+        _binding = null
     }
 
 }
