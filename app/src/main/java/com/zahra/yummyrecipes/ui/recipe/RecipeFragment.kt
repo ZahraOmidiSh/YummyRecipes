d43051061e05c02bb7273ac80396df3d1c96087f
Zahra Omidi <Zahram@gm.com>
2023-07-26 17:46:09 +0330

    update Good morning , ...

--- app/src/main/java/com/zahra/yummyrecipes/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/recipe/RecipeFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.zahra.yummyrecipes.R
 import com.zahra.yummyrecipes.databinding.FragmentRecipeBinding
+import com.zahra.yummyrecipes.viewmodel.RegisterViewModel
+import com.zahra.yummyrecipes.viewmodel.TimeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.system.exitProcess
 
 
 @AndroidEntryPoint
@@ -43,18 +48,19 @@ class RecipeFragment : Fragment() {
 //            timeTxt.text=current
 
             timeViewModel.currentTime.observe(viewLifecycleOwner) {
-            var currentTime = it.toInt()
-                if(currentTime in 7..12){
-                    timeTxt.text="GoodMorning"
-                }
-                else if(currentTime in 12..17){
-                    timeTxt.text="GoodAfternoon"
-                }
-                else if(currentTime in 17..20){
-                    timeTxt.text="GoodEvening"
-                }
-                else {
-                    timeTxt.text="GoodNight"
+            when (it.toInt()) {
+                    in 7..12 -> {
+                        timeTxt.text=getString(R.string.goodmorning)
+                    }
+                    in 12..17 -> {
+                        timeTxt.text=getString(R.string.goodafternoon)
+                    }
+                    in 17..20 -> {
+                        timeTxt.text=getString(R.string.goodevening)
+                    }
+                    else -> {
+                        timeTxt.text=getString(R.string.goodnight)
+                    }
                 }
             }
 
