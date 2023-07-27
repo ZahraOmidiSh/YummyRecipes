5bef62445a895011f9c7546657e308ba5c7f10eb
Zahra Omidi <Zahram@gm.com>
2023-07-27 12:57:47 +0330

    success to add greeting but problem on theme change

--- app/src/main/java/com/zahra/yummyrecipes/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/recipe/RecipeFragment.kt
@@ -1,17 +1,22 @@
 package com.zahra.yummyrecipes.ui.recipe
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.withStarted
 import com.zahra.yummyrecipes.R
 import com.zahra.yummyrecipes.databinding.FragmentRecipeBinding
 import com.zahra.yummyrecipes.utils.Constants.MY_QUOTES_API_KEY
 import com.zahra.yummyrecipes.viewmodel.RecipeViewModel
 import com.zahra.yummyrecipes.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import java.util.Calendar
 
 
@@ -37,43 +42,47 @@ class RecipeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //InitViews
-        binding.apply {
-
-//            val current = LocalDateTime.now()
-
-            val calendar = Calendar.getInstance()
-            val hour = calendar.get(Calendar.HOUR_OF_DAY)
-
-            /*
-            when (hour) {
-                in 7..12 -> {
-                    timeTxt.text = getString(R.string.goodMorning)
-                }
-
-                in 12..17 -> {
-                    timeTxt.text = getString(R.string.goodAfternoon)
-                }
-
-                in 17..20 -> {
-                    timeTxt.text = getString(R.string.goodEvening)
-                }
-
-                else -> {
-                    timeTxt.text = getString(R.string.goodNight)
-                }
-            }
-            */
-            callQuotesApi()
-
-        }
+        //Show username
+        lifecycleScope.launchWhenCreated { showUsername() }
 
     }
 
-    private fun callQuotesApi(){
-        recipeViewModel.callQuoteApi(MY_QUOTES_API_KEY,"food")
-        recipeViewModel.quoteData.observe(viewLifecycleOwner) {
-            binding.timeTxt.text =it.data.toString()
+
+    @SuppressLint("SetTextI18n")
+    suspend fun showUsername(){
+        registerViewModel.readData.collect{
+            binding.usernameTxt.text =
+                "${getGreeting()},${it.username} ${getEmojiByUnicode()}"
+
         }
+    }
+
+    private fun getGreeting():String{
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        var greeting=""
+
+        when (hour) {
+            in 7..12 -> {
+                greeting = getString(R.string.goodMorning)
+            }
+
+            in 12..17 -> {
+                greeting= getString(R.string.goodAfternoon)
+            }
+
+            in 17..20 -> {
+                greeting= getString(R.string.goodEvening)
+            }
+
+            else -> {
+                greeting= getString(R.string.goodNight)
+            }
+        }
+        return greeting
+    }
+    private fun getEmojiByUnicode():String {
+        return String(Character.toChars(0x1f44b))
     }
 
     override fun onDestroy() {
