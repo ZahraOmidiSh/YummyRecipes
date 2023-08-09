6438c23429d9e60c1f7153c036d3ccfa76e9201b
Zahra Omidi <Zahram@gm.com>
2023-08-09 04:01:20 +0330

    add recipe fragment styles to themes

--- app/src/main/java/com/zahra/yummyrecipes/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/recipe/RecipeFragment.kt
@@ -1,6 +1,7 @@
 package com.zahra.yummyrecipes.ui.recipe
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
