c648c197631592acb23c922b29758c234df120e8
Zahra Omidi <Zahram@gm.com>
2023-08-22 11:56:19 +0330

    detail page add equipment adapter

---
+++ app/src/main/java/com/zahra/yummyrecipes/adapter/EquipmentsAdapter.kt
@@ -0,0 +1,65 @@
+package com.zahra.yummyrecipes.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.request.CachePolicy
+import com.zahra.yummyrecipes.R
+import com.zahra.yummyrecipes.databinding.ItemEquipmentBinding
+import com.zahra.yummyrecipes.databinding.ItemIngredientsBinding
+import com.zahra.yummyrecipes.databinding.ItemRecyclerViewBinding
+import com.zahra.yummyrecipes.models.detail.ResponseDetail
+import com.zahra.yummyrecipes.models.detail.ResponseDetail.AnalyzedInstruction.Step.Equipment
+import com.zahra.yummyrecipes.models.detail.ResponseDetail.ExtendedIngredient
+import com.zahra.yummyrecipes.models.recipe.ResponseRecipes.Result
+import com.zahra.yummyrecipes.utils.BaseDiffUtils
+import com.zahra.yummyrecipes.utils.Constants.NEW_IMAGE_SIZE
+import com.zahra.yummyrecipes.utils.Constants.OLD_IMAGE_SIZE
+import com.zahra.yummyrecipes.utils.minToHour
+import javax.inject.Inject
+
+class EquipmentsAdapter @Inject constructor() : RecyclerView.Adapter<EquipmentsAdapter.ViewHolder>() {
+    private lateinit var binding: ItemEquipmentBinding
+    private var items = emptyList<Equipment>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = ItemEquipmentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position])
+
+    override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int) = position
+
+    override fun getItemId(position: Int) = position.toLong()
+    inner class ViewHolder : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
+        fun bind(item: Equipment) {
+            binding.apply {
+                //Text
+                equipmentNameTxt.text = item.name
+                //Image
+                val imageSize = item.image!!.replace(OLD_IMAGE_SIZE, NEW_IMAGE_SIZE)
+                equipmentImg.load(imageSize) {
+                    crossfade(true)
+                    crossfade(500)
+                    memoryCachePolicy(CachePolicy.ENABLED)
+                    error(R.drawable.kitchen_set)
+                }
+            }
+        }
+    }
+
+
+    fun setData(data: List<Equipment>) {
+        val adapterDiffUtils = BaseDiffUtils(items, data)
+        val diffUtils = DiffUtil.calculateDiff(adapterDiffUtils)
+        items = data
+        diffUtils.dispatchUpdatesTo(this)
+    }
+}
