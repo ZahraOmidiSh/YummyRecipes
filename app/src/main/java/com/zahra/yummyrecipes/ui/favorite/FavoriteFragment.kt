23700b903cbc96f3cead83651cec7d228d275181
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-09-19 02:44:11 +0330

    coding in favorite fragment

--- app/src/main/java/com/zahra/yummyrecipes/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/favorite/FavoriteFragment.kt
@@ -4,15 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.zahra.yummyrecipes.adapter.FavoriteAdapter
 import com.zahra.yummyrecipes.databinding.FragmentFavoriteBinding
+import com.zahra.yummyrecipes.ui.recipe.RecipeFragmentDirections
+import com.zahra.yummyrecipes.utils.isVisible
+import com.zahra.yummyrecipes.utils.setupRecyclerview
+import com.zahra.yummyrecipes.viewmodel.FavoriteViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class FavoriteFragment : Fragment() {
     //Binding
     private var _binding: FragmentFavoriteBinding? = null
     private val binding get() = _binding!!
+
+    @Inject
+    lateinit var favoriteAdapter: FavoriteAdapter
+
+    //Other
+    private val viewModel: FavoriteViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +44,27 @@ class FavoriteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         //InitViews
         binding.apply {
+            //Load favorites
+            viewModel.readFavoriteData.observe(viewLifecycleOwner) {
+                if (it.isNotEmpty()) {
+                    emptyTxt.isVisible(false, favoriteList)
+                    //Data
+                    favoriteAdapter.setData(it)
+                    favoriteList.apply {
+                        layoutManager =
+                            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                        adapter = favoriteAdapter
+                    }
+                    //Click
+                    favoriteAdapter.setonItemClickListener { id ->
+                        val action = RecipeFragmentDirections.actionToDetail(id)
+                        findNavController().navigate(action)
+                    }
+
+                } else {
+                    emptyTxt.isVisible(true, favoriteList)
+                }
+            }
 
         }
     }
