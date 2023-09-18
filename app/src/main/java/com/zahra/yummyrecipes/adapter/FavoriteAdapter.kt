a35f0f184a00e719721df3f114e63fd9873f5c7e
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-09-19 02:31:37 +0330

    add FavoriteAdapter

---
+++ app/src/main/java/com/zahra/yummyrecipes/adapter/FavoriteAdapter.kt
@@ -0,0 +1,99 @@
+package com.zahra.yummyrecipes.adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.request.CachePolicy
+import com.zahra.yummyrecipes.R
+import com.zahra.yummyrecipes.data.database.entity.FavoriteEntity
+import com.zahra.yummyrecipes.databinding.ItemRecyclerViewBinding
+import com.zahra.yummyrecipes.utils.BaseDiffUtils
+import com.zahra.yummyrecipes.utils.Constants.NEW_IMAGE_SIZE
+import com.zahra.yummyrecipes.utils.Constants.OLD_IMAGE_SIZE
+import com.zahra.yummyrecipes.utils.minToHour
+import javax.inject.Inject
+
+class FavoriteAdapter @Inject constructor() : RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
+    private lateinit var binding: ItemRecyclerViewBinding
+    private var items = emptyList<FavoriteEntity>()
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding =
+            ItemRecyclerViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        context = parent.context
+        return ViewHolder()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position])
+
+    override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int) = position
+
+    override fun getItemId(position: Int) = position.toLong()
+
+    override fun onViewAttachedToWindow(holder: ViewHolder) {
+        super.onViewAttachedToWindow(holder)
+        holder.initAnimation()
+    }
+
+    override fun onViewDetachedFromWindow(holder: ViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        holder.clearAnimation()
+    }
+
+    inner class ViewHolder : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
+        fun bind(item: FavoriteEntity) {
+            binding.apply {
+                item.result.let { result ->
+                    //Text
+                    suggestedTitleTxt.text = result.title
+                    suggestedTimeTxt.text = result.readyInMinutes!!.minToHour()
+                    suggestedHeartTxt.text = result.aggregateLikes.toString()
+                    //Image
+                    val imageSize = result.image!!.replace(OLD_IMAGE_SIZE, NEW_IMAGE_SIZE)
+                    suggestedImg.load(imageSize) {
+                        crossfade(true)
+                        crossfade(800)
+                        memoryCachePolicy(CachePolicy.ENABLED)
+                        error(R.drawable.salad)
+                    }
+                    //Click
+                    root.setOnClickListener {
+                        onItemClickListener?.let { it(item.id!!) }
+
+                    }
+                }
+            }
+        }
+
+        fun initAnimation() {
+            binding.root.animation = AnimationUtils.loadAnimation(context, R.anim.item_anim)
+        }
+
+        fun clearAnimation() {
+            binding.root.clearAnimation()
+        }
+
+    }
+
+    var onItemClickListener: ((Int) -> Unit)? = null
+
+    fun setonItemClickListener(listener: (Int) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    fun setData(data: List<FavoriteEntity>) {
+        val adapterDiffUtils = BaseDiffUtils(items, data)
+        val diffUtils = DiffUtil.calculateDiff(adapterDiffUtils)
+        items = data
+        diffUtils.dispatchUpdatesTo(this)
+    }
+}
