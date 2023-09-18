27a89eb84507d485d5280308d46f1b99158c9a73
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-09-18 20:19:46 +0330

    update RecipeAppDatabase

--- app/src/main/java/com/zahra/yummyrecipes/data/database/RecipeAppDatabase.kt
+++ app/src/main/java/com/zahra/yummyrecipes/data/database/RecipeAppDatabase.kt
@@ -4,9 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.zahra.yummyrecipes.data.database.entity.DetailEntity
+import com.zahra.yummyrecipes.data.database.entity.FavoriteEntity
 import com.zahra.yummyrecipes.data.database.entity.RecipeEntity
 
-@Database(entities = [RecipeEntity::class , DetailEntity::class], version = 2 , exportSchema = false)
+@Database(entities = [RecipeEntity::class , DetailEntity::class,FavoriteEntity::class], version = 3 , exportSchema = false)
 @TypeConverters(RecipeAppTypeConverter::class)
 abstract class RecipeAppDatabase:RoomDatabase() {
     abstract fun dao():RecipeAppDao
