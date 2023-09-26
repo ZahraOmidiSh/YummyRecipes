cd0665ab86e59ef8c6a4727cda10c1621b0f998a
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-09-26 16:19:25 +0330

    add SearchIngredientsAdapter

---
+++ app/src/main/java/com/zahra/yummyrecipes/adapter/SearchIngredientsAdapter.kt
@@ -0,0 +1,60 @@
+package com.zahra.yummyrecipes.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.request.CachePolicy
+import com.zahra.yummyrecipes.R
+import com.zahra.yummyrecipes.databinding.ItemIngredientsSearchBinding
+import com.zahra.yummyrecipes.models.detail.ResponseDetail.ExtendedIngredient
+import com.zahra.yummyrecipes.models.search.IngredientsModel
+import com.zahra.yummyrecipes.utils.BaseDiffUtils
+import com.zahra.yummyrecipes.utils.Constants.BASE_URL_IMAGE_INGREDIENTS
+import javax.inject.Inject
+
+class SearchIngredientsAdapter @Inject constructor() :
+    RecyclerView.Adapter<SearchIngredientsAdapter.ViewHolder>() {
+    private lateinit var binding: ItemIngredientsSearchBinding
+    private var items = emptyList<IngredientsModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = ItemIngredientsSearchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position])
+
+    override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int) = position
+
+    override fun getItemId(position: Int) = position.toLong()
+    inner class ViewHolder : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
+        fun bind(item: IngredientsModel) {
+            binding.apply {
+                //Text
+                ingredientNameTxt.text = item.ingredientsName
+                //Image
+                val image = item.ingredientsImg
+                ingredientImg.load(image) {
+                    crossfade(true)
+                    crossfade(500)
+                    memoryCachePolicy(CachePolicy.ENABLED)
+                    error(R.drawable.bg_rounded_white)
+                }
+            }
+        }
+    }
+
+
+    fun setData(data: List<IngredientsModel>) {
+        val adapterDiffUtils = BaseDiffUtils(items, data)
+        val diffUtils = DiffUtil.calculateDiff(adapterDiffUtils)
+        items = data
+        diffUtils.dispatchUpdatesTo(this)
+    }
+}
