cbc0643f620101948b8e9481bf999999f21f0289
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-09-26 15:46:15 +0330

    add IngredientsModel

---
+++ app/src/main/java/com/zahra/yummyrecipes/models/search/IngredientsModel.kt
@@ -0,0 +1,9 @@
+package com.zahra.yummyrecipes.models.search
+
+import com.zahra.yummyrecipes.R
+
+data class IngredientsModel(
+    var ingredientsId:Int=0,
+    var ingredientsName:String="",
+    var ingredientsImg:Int= 0
+)
