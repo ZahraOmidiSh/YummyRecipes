de597223f0202c08d1f43a1b53673b8e0c90ddaf
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-09-26 16:02:16 +0330

    update IngredientsModel

--- app/src/main/java/com/zahra/yummyrecipes/models/search/IngredientsModel.kt
+++ app/src/main/java/com/zahra/yummyrecipes/models/search/IngredientsModel.kt
@@ -1,8 +1,10 @@
 package com.zahra.yummyrecipes.models.search
 
+import androidx.room.PrimaryKey
 import com.zahra.yummyrecipes.R
 
 data class IngredientsModel(
+    @PrimaryKey(autoGenerate = true)
     var ingredientsId:Int=0,
     var ingredientsName:String="",
     var ingredientsImg:Int= 0
