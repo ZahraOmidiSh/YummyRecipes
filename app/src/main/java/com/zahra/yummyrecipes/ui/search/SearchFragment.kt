18416725a56872d19a56bb5db4acbfe71d867fd9
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-09-26 16:29:34 +0330

    first attempt on adding ingredients list to fragment

--- app/src/main/java/com/zahra/yummyrecipes/ui/search/SearchFragment.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/search/SearchFragment.kt
@@ -5,14 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zahra.yummyrecipes.adapter.SearchIngredientsAdapter
 import com.zahra.yummyrecipes.databinding.FragmentSearchBinding
+import com.zahra.yummyrecipes.models.search.IngredientsModel
+import com.zahra.yummyrecipes.utils.setupRecyclerview
+import com.zahra.yummyrecipes.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
     //Binding
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+
+
+    @Inject
+    private lateinit var searchIngredientsAdapter: SearchIngredientsAdapter
+
+    //Others
+    private val viewModel:SearchViewModel by viewModels()
+    private val searchIngredientsList:MutableList<IngredientsModel> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +42,14 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         //InitViews
         binding.apply {
+            viewModel.loadIngredientsList()
+            viewModel.IngredientsList.observe(viewLifecycleOwner){
+                searchIngredientsList.addAll(it)
+                ingredientsList.setupRecyclerview(
+                    LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false),
+                    searchIngredientsAdapter)
+            }
+
 
         }
     }
