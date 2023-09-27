3e346cada469df598602772c1bb9bc78ef6383b3
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-09-27 15:32:41 +0330

    add ingredient pics to  drawable

--- app/src/main/java/com/zahra/yummyrecipes/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/zahra/yummyrecipes/viewmodel/SearchViewModel.kt
@@ -17,19 +17,18 @@ class SearchViewModel@Inject constructor(): ViewModel() {
     val tenIngredientsList = MutableLiveData<MutableList<IngredientsModel>>()
 
     fun loadTenIngredientsList() = viewModelScope.launch(Dispatchers.IO) {
-        val carrot = IngredientsModel(0,"Carrot", R.drawable.carrot)
-        val chicken = IngredientsModel(0,"Chicken", R.drawable.chicken)
-        val egg = IngredientsModel(0,"Egg", R.drawable.egg)
-//        val pasta = IngredientsModel(0,"pasta", R.drawable.pasta)
-//        val apple = IngredientsModel(0,"apple", R.drawable.apple)
-//        val banana = IngredientsModel(0,"banana", R.drawable.banana)
-//        val cheese = IngredientsModel(0,"cheese", R.drawable.cheese)
-//        val rice = IngredientsModel(0,"rice", R.drawable.rice)
-//        val milk = IngredientsModel(0,"milk", R.drawable.milk)
-//        val fish = IngredientsModel(0,"fish", R.drawable.fish)
+        val carrot = IngredientsModel(0,"Carrot", R.drawable.s_carrot)
+        val chicken = IngredientsModel(0,"Chicken", R.drawable.s_chicken)
+        val egg = IngredientsModel(0,"Egg", R.drawable.s_egg)
+        val pasta = IngredientsModel(0,"Pasta", R.drawable.s_pasta)
+        val apple = IngredientsModel(0,"Apple", R.drawable.s_apple)
+        val banana = IngredientsModel(0,"Banana", R.drawable.s_banana)
+        val cheese = IngredientsModel(0,"Cheese", R.drawable.s_cheese)
+        val rice = IngredientsModel(0,"Rice", R.drawable.s_rice)
+        val milk = IngredientsModel(0,"Milk", R.drawable.s_milk)
+        val fish = IngredientsModel(0,"Fish", R.drawable.s_fish)
 
-//        val data = mutableListOf(carrot, chicken, egg,pasta,apple,banana,cheese,rice,milk,fish)
-        val data = mutableListOf(carrot, chicken, egg)
+        val data = mutableListOf(carrot, chicken, egg,pasta,apple,banana,cheese,rice,milk,fish)
         tenIngredientsList.postValue(data)
     }
 }
