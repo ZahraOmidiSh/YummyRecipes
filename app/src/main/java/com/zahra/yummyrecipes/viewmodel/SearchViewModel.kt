0cb929422f414783790915cd07f8e2c5f8e764ac
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-09-27 18:24:12 +0330

    add loadExpandedIngredientsList function to SearchViewModel

--- app/src/main/java/com/zahra/yummyrecipes/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/zahra/yummyrecipes/viewmodel/SearchViewModel.kt
@@ -12,23 +12,64 @@ import javax.inject.Inject
 
 @HiltViewModel
 
-class SearchViewModel@Inject constructor(): ViewModel() {
+class SearchViewModel @Inject constructor() : ViewModel() {
     //Ingredients
+    //Limited
     val limitIngredientsList = MutableLiveData<MutableList<IngredientsModel>>()
-
     fun loadLimitIngredientsList() = viewModelScope.launch(Dispatchers.IO) {
-        val carrot = IngredientsModel(0,"Carrot", R.drawable.s_carrot)
-        val chicken = IngredientsModel(0,"Chicken", R.drawable.s_chicken)
-        val egg = IngredientsModel(0,"Egg", R.drawable.s_egg)
-        val pasta = IngredientsModel(0,"Pasta", R.drawable.s_pasta)
-        val apple = IngredientsModel(0,"Apple", R.drawable.s_apple)
-        val banana = IngredientsModel(0,"Banana", R.drawable.s_banana)
-        val cheese = IngredientsModel(0,"Cheese", R.drawable.s_cheese)
-        val rice = IngredientsModel(0,"Rice", R.drawable.s_rice)
-        val milk = IngredientsModel(0,"Milk", R.drawable.s_milk)
-        val fish = IngredientsModel(0,"Fish", R.drawable.s_fish)
+        val carrot = IngredientsModel(0, "Carrot", R.drawable.s_carrot)
+        val chicken = IngredientsModel(0, "Chicken", R.drawable.s_chicken)
+        val egg = IngredientsModel(0, "Egg", R.drawable.s_egg)
+        val pasta = IngredientsModel(0, "Pasta", R.drawable.s_pasta)
+        val apple = IngredientsModel(0, "Apple", R.drawable.s_apple)
+        val banana = IngredientsModel(0, "Banana", R.drawable.s_banana)
+        val cheese = IngredientsModel(0, "Cheese", R.drawable.s_cheese)
+        val rice = IngredientsModel(0, "Rice", R.drawable.s_rice)
+        val milk = IngredientsModel(0, "Milk", R.drawable.s_milk)
+        val fish = IngredientsModel(0, "Fish", R.drawable.s_fish)
 
-        val data = mutableListOf(carrot, chicken, egg,pasta,apple,banana,cheese,rice,milk,fish)
+        val data =
+            mutableListOf(carrot, chicken, egg, pasta, apple, banana, cheese, rice, milk, fish)
         limitIngredientsList.postValue(data)
+    }
+
+    //Expanded
+    val expandedIngredientsList = MutableLiveData<MutableList<IngredientsModel>>()
+    fun loadExpandedIngredientsList() = viewModelScope.launch(Dispatchers.IO) {
+        val carrot = IngredientsModel(0, "Carrot", R.drawable.s_carrot)
+        val chicken = IngredientsModel(0, "Chicken", R.drawable.s_chicken)
+        val egg = IngredientsModel(0, "Egg", R.drawable.s_egg)
+        val pasta = IngredientsModel(0, "Pasta", R.drawable.s_pasta)
+        val apple = IngredientsModel(0, "Apple", R.drawable.s_apple)
+        val banana = IngredientsModel(0, "Banana", R.drawable.s_banana)
+        val cheese = IngredientsModel(0, "Cheese", R.drawable.s_cheese)
+        val rice = IngredientsModel(0, "Rice", R.drawable.s_rice)
+        val milk = IngredientsModel(0, "Milk", R.drawable.s_milk)
+        val fish = IngredientsModel(0, "Fish", R.drawable.s_fish)
+
+
+        val data = mutableListOf(
+            carrot,
+            chicken,
+            egg,
+            pasta,
+            apple,
+            banana,
+            cheese,
+            rice,
+            milk,
+            fish,
+            carrot,
+            chicken,
+            egg,
+            pasta,
+            apple,
+            banana,
+            cheese,
+            rice,
+            milk,
+            fish
+        )
+        expandedIngredientsList.postValue(data)
     }
 }
