b485be312071ebe093a96a8e1cb1ddc47527683d
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-10-28 11:46:04 +0330

    add @PrimaryKey(autoGenerate = false)

--- app/src/main/java/com/zahra/yummyrecipes/models/search/IngredientsModel.kt
+++ app/src/main/java/com/zahra/yummyrecipes/models/search/IngredientsModel.kt
@@ -4,6 +4,7 @@ import androidx.room.PrimaryKey
 import com.zahra.yummyrecipes.R
 
 data class IngredientsModel(
+    @PrimaryKey(autoGenerate = false)
     var ingredientsName:String="",
     var ingredientsImg:Int= 0,
     var isSelected:Boolean =false
