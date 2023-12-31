1ecb6ed72fe5c927f897f4e9b8bf292666be457a
Zahra Omidi <zahraomidi.sh@outlook.com>
2023-12-31 16:11:28 +0330

    add MealPlannerViewModel

--- app/src/main/java/com/zahra/yummyrecipes/viewmodel/MealPlannerViewModel.kt
+++ app/src/main/java/com/zahra/yummyrecipes/viewmodel/MealPlannerViewModel.kt
@@ -2,7 +2,8 @@ package com.zahra.yummyrecipes.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MealPlannerViewModel:ViewModel() {
+class MealPlannerViewModel @Inject constructor() : ViewModel() {
 }
