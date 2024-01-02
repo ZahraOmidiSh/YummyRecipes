7cf379427c0bdb1a2dff53387fc7a6b5e9e4ceda
Zahra Omidi <zahraomidi.sh@outlook.com>
2024-01-02 19:23:44 +0330

    add meal part to RecipeAppDao

--- app/src/main/java/com/zahra/yummyrecipes/data/database/RecipeAppDao.kt
+++ app/src/main/java/com/zahra/yummyrecipes/data/database/RecipeAppDao.kt
@@ -7,6 +7,7 @@ import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
 import com.zahra.yummyrecipes.data.database.entity.DetailEntity
 import com.zahra.yummyrecipes.data.database.entity.FavoriteEntity
+import com.zahra.yummyrecipes.data.database.entity.MealPlannerEntity
 import com.zahra.yummyrecipes.data.database.entity.RecipeEntity
 import com.zahra.yummyrecipes.utils.Constants.DETAIL_TABLE_NAME
 import com.zahra.yummyrecipes.utils.Constants.FAVORITE_TABLE_NAME
@@ -17,7 +18,7 @@ import kotlinx.coroutines.flow.Flow
 interface RecipeAppDao {
     //Recipes
     @Insert(onConflict = REPLACE)
-    suspend fun saveRecipes(entity:RecipeEntity)
+    suspend fun saveRecipes(entity: RecipeEntity)
 
     @Query("SELECT * FROM $RECIPE_TABLE_NAME ORDER BY ID ASC")
     fun loadRecipes(): Flow<List<RecipeEntity>>
@@ -27,21 +28,32 @@ interface RecipeAppDao {
     suspend fun saveDetail(entity: DetailEntity)
 
     @Query("SELECT * FROM $DETAIL_TABLE_NAME WHERE id = :id")
-    fun loadDetail(id:Int):Flow<DetailEntity>
+    fun loadDetail(id: Int): Flow<DetailEntity>
 
     @Query("SELECT EXISTS (SELECT 1 FROM $DETAIL_TABLE_NAME WHERE ID = :id)")
-    fun existsDetail(id:Int):Flow<Boolean>
+    fun existsDetail(id: Int): Flow<Boolean>
 
     //Favorite
     @Insert(onConflict = REPLACE)
-    suspend fun saveFavorite(entity:FavoriteEntity)
+    suspend fun saveFavorite(entity: FavoriteEntity)
 
     @Delete
-    suspend fun deleteFavorite(entity:FavoriteEntity)
+    suspend fun deleteFavorite(entity: FavoriteEntity)
 
     @Query("SELECT * FROM $FAVORITE_TABLE_NAME ORDER BY ID ASC")
-    fun loadFavorite():Flow<List<FavoriteEntity>>
+    fun loadFavorite(): Flow<List<FavoriteEntity>>
 
     @Query("SELECT EXISTS (SELECT 1 FROM $FAVORITE_TABLE_NAME WHERE ID = :id)")
-    fun existsFavorite(id:Int):Flow<Boolean>
+    fun existsFavorite(id: Int): Flow<Boolean>
+
+    //Meals
+    @Insert(onConflict = REPLACE)
+    suspend fun saveMeal(entity: MealPlannerEntity)
+
+    @Delete
+    suspend fun deleteMeal(entity: MealPlannerEntity)
+
+    @Query("SELECT * FROM $FAVORITE_TABLE_NAME WHERE id LIKE :date || '%'")
+    fun loadMeal(date: String): Flow<List<MealPlannerEntity>>
+
 }
