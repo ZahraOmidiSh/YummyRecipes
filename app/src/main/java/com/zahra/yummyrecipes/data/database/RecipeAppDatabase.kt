d20266bc5614a5a4b763e5ab9bcf7fe452de96bd
Zahra Omidi <zahraomidi.sh@outlook.com>
2024-01-02 19:25:28 +0330

    add  MealPlannerEntity::class to RecipeAppDatabase

--- app/src/main/java/com/zahra/yummyrecipes/data/database/RecipeAppDatabase.kt
+++ app/src/main/java/com/zahra/yummyrecipes/data/database/RecipeAppDatabase.kt
@@ -5,10 +5,15 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.zahra.yummyrecipes.data.database.entity.DetailEntity
 import com.zahra.yummyrecipes.data.database.entity.FavoriteEntity
+import com.zahra.yummyrecipes.data.database.entity.MealPlannerEntity
 import com.zahra.yummyrecipes.data.database.entity.RecipeEntity
 
-@Database(entities = [RecipeEntity::class , DetailEntity::class,FavoriteEntity::class], version = 3 , exportSchema = false)
+@Database(
+    entities = [RecipeEntity::class, DetailEntity::class, FavoriteEntity::class, MealPlannerEntity::class],
+    version = 3,
+    exportSchema = false
+)
 @TypeConverters(RecipeAppTypeConverter::class)
-abstract class RecipeAppDatabase:RoomDatabase() {
-    abstract fun dao():RecipeAppDao
+abstract class RecipeAppDatabase : RoomDatabase() {
+    abstract fun dao(): RecipeAppDao
 }
