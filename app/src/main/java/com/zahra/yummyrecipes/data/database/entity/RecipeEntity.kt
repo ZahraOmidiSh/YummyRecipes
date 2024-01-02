9e37196a5c1fbd44e6ebc14e46ca43d3ef1e9a46
Zahra Omidi <zahraomidi.sh@outlook.com>
2024-01-02 19:23:14 +0330

    change id type to string

--- app/src/main/java/com/zahra/yummyrecipes/data/database/entity/RecipeEntity.kt
+++ app/src/main/java/com/zahra/yummyrecipes/data/database/entity/RecipeEntity.kt
@@ -8,6 +8,6 @@ import com.zahra.yummyrecipes.utils.Constants.RECIPE_TABLE_NAME
 @Entity(tableName = RECIPE_TABLE_NAME)
 data class RecipeEntity(
     @PrimaryKey(autoGenerate = false)
-    var id: Int = 0,
+    var id: String ,
     var response: ResponseRecipes
 )
