abd5a141bf063d02533af0d3b4f37e90c42409c7
Zahra Omidi <zahraomidi.sh@outlook.com>
2024-01-03 15:19:08 +0330

    add Meal part to MealPlannerViewModel

--- app/src/main/java/com/zahra/yummyrecipes/viewmodel/MealPlannerViewModel.kt
+++ app/src/main/java/com/zahra/yummyrecipes/viewmodel/MealPlannerViewModel.kt
@@ -1,10 +1,14 @@
 package com.zahra.yummyrecipes.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.zahra.yummyrecipes.data.repository.FavoriteRepository
+import com.zahra.yummyrecipes.data.repository.MealRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MealPlannerViewModel @Inject constructor() : ViewModel() {
-
+class MealPlannerViewModel @Inject constructor(repository: MealRepository) : ViewModel() {
+    val data = "Monday"
+    val readPlannedMealData = repository.local.loadPlannedMeals(data).asLiveData()
 }
