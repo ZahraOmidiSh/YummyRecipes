acc757a9271749ea30428b0d465119d2fab8ff5c
Zahra Omidi <zahraomidi.sh@outlook.com>
2024-01-03 15:09:53 +0330

    add Meal part to LocalDataSource

--- app/src/main/java/com/zahra/yummyrecipes/data/source/LocalDataSource.kt
+++ app/src/main/java/com/zahra/yummyrecipes/data/source/LocalDataSource.kt
@@ -3,6 +3,7 @@ package com.zahra.yummyrecipes.data.source
 import com.zahra.yummyrecipes.data.database.RecipeAppDao
 import com.zahra.yummyrecipes.data.database.entity.DetailEntity
 import com.zahra.yummyrecipes.data.database.entity.FavoriteEntity
+import com.zahra.yummyrecipes.data.database.entity.MealPlannerEntity
 import com.zahra.yummyrecipes.data.database.entity.RecipeEntity
 import javax.inject.Inject
 
@@ -18,8 +19,13 @@ class LocalDataSource @Inject constructor(private val dao: RecipeAppDao) {
 
     //Favorite
     suspend fun saveFavorite(entity: FavoriteEntity) = dao.saveFavorite(entity)
-    suspend fun deleteFavorite(entity:FavoriteEntity) = dao.deleteFavorite(entity)
+    suspend fun deleteFavorite(entity: FavoriteEntity) = dao.deleteFavorite(entity)
     fun loadFavorite() = dao.loadFavorite()
     fun existsFavorite(id: Int) = dao.existsFavorite(id)
+
+    //Meal
+    suspend fun savePlannedMeal(entity: MealPlannerEntity) = dao.saveMeal(entity)
+    suspend fun deletePlannedMeal(entity: MealPlannerEntity) = dao.deleteMeal(entity)
+    fun loadPlannedMeals(date: String) = dao.loadMeal(date)
 
 }
