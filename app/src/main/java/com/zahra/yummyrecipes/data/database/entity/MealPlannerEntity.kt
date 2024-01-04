646277a472cfa09a226223df464d497562204263
Zahra Omidi <zahraomidi.sh@outlook.com>
2024-01-04 20:48:24 +0330

    change response to ResponseDetail

--- app/src/main/java/com/zahra/yummyrecipes/data/database/entity/MealPlannerEntity.kt
+++ app/src/main/java/com/zahra/yummyrecipes/data/database/entity/MealPlannerEntity.kt
@@ -2,12 +2,12 @@ package com.zahra.yummyrecipes.data.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.zahra.yummyrecipes.models.recipe.ResponseRecipes
+import com.zahra.yummyrecipes.models.detail.ResponseDetail
 import com.zahra.yummyrecipes.utils.Constants
 
 @Entity(tableName = Constants.MEAL_PLANNER_TABLE_NAME)
 data class MealPlannerEntity(
     @PrimaryKey(autoGenerate = false)
     var id: Int = 0,
-    var response: ResponseRecipes
+    var response: ResponseDetail
 )
