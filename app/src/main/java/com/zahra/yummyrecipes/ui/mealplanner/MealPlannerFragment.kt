ff44bb071d64ebac2b3db527899a02e5d56c828a
Zahra Omidi <zahraomidi.sh@outlook.com>
2024-01-04 11:49:08 +0330

    add calendar part to MealPlannerFragment

--- app/src/main/java/com/zahra/yummyrecipes/ui/mealplanner/MealPlannerFragment.kt
+++ app/src/main/java/com/zahra/yummyrecipes/ui/mealplanner/MealPlannerFragment.kt
@@ -1,5 +1,6 @@
 package com.zahra.yummyrecipes.ui.mealplanner
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.zahra.yummyrecipes.databinding.FragmentMealPlannerBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 @AndroidEntryPoint
 class MealPlannerFragment : Fragment() {
@@ -23,11 +27,36 @@ class MealPlannerFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //InitViews
         binding.apply {
+            //find the current week
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.DAY_OF_WEEK,calendar.firstDayOfWeek)
+            val dateFormat = SimpleDateFormat("MM-dd", Locale.getDefault())
+            val startDate = dateFormat.format(calendar.time)
 
+            calendar.add(Calendar.DAY_OF_WEEK,6)
+
+            val endDate = dateFormat.format(calendar.time)
+
+            //Display the dates in TextViews
+            monday.text="monday:$startDate"
+            sunday.text="monday:$endDate"
+
+
+        }
+    }
+
+    private fun getDatesBetween(startDate:Calendar,endDate:Calendar):List<Calendar>{
+        val dates = mutableListOf<Calendar>()
+        val calendar =startDate.clone() as Calendar
+
+        while (calendar.before(endDate) || calendar == endDate){
+            dates.add(calendar.clone() as Calendar)
+            calendar.add(Calendar.DAY_OF_WEEK , 1)
         }
     }
 
