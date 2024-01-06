3f273b0d1821ee7aeda534dcd2ae3b33a1137bba
Zahra Omidi <zahraomidi.sh@outlook.com>
2024-01-07 00:45:10 +0330

    add fun setCalendar() to MealPlannerViewModel

--- app/src/main/java/com/zahra/yummyrecipes/viewmodel/MealPlannerViewModel.kt
+++ app/src/main/java/com/zahra/yummyrecipes/viewmodel/MealPlannerViewModel.kt
@@ -5,10 +5,21 @@ import androidx.lifecycle.asLiveData
 import com.zahra.yummyrecipes.data.repository.FavoriteRepository
 import com.zahra.yummyrecipes.data.repository.MealRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.util.Calendar
 import javax.inject.Inject
 
 @HiltViewModel
 class MealPlannerViewModel @Inject constructor(repository: MealRepository) : ViewModel() {
     val data = "Monday"
     val readPlannedMealData = repository.local.loadPlannedMeals(data).asLiveData()
+
+
+    //setWeek calendar
+    fun setCalendar(){
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.DAY_OF_WEEK,calendar.firstDayOfWeek)
+    }
+
+
+
 }
