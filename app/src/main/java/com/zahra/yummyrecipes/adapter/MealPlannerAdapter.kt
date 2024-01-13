49c9a98f3bb04227cc10122ba803c5f36963f0bb
Zahra Omidi <zahraomidi.sh@outlook.com>
2024-01-13 16:41:32 +0330

    changes in adapter

--- app/src/main/java/com/zahra/yummyrecipes/adapter/MealPlannerAdapter.kt
+++ app/src/main/java/com/zahra/yummyrecipes/adapter/MealPlannerAdapter.kt
@@ -10,7 +10,9 @@ import coil.request.CachePolicy
 import com.zahra.yummyrecipes.R
 import com.zahra.yummyrecipes.data.database.entity.MealPlannerEntity
 import com.zahra.yummyrecipes.databinding.ItemSimilarBinding
+import com.zahra.yummyrecipes.models.detail.ResponseDetail
 import com.zahra.yummyrecipes.utils.BaseDiffUtils
+import com.zahra.yummyrecipes.utils.Constants
 import com.zahra.yummyrecipes.utils.Constants.NEW_IMAGE_SIZE
 import com.zahra.yummyrecipes.utils.Constants.OLD_IMAGE_SIZE
 import javax.inject.Inject
@@ -18,7 +20,7 @@ import javax.inject.Inject
 class MealPlannerAdapter @Inject constructor() :
     RecyclerView.Adapter<MealPlannerAdapter.ViewHolder>() {
     private lateinit var binding: ItemSimilarBinding
-    private var items = emptyList<MealPlannerEntity>()
+    private var items = emptyList<ResponseDetail>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         binding =
@@ -36,41 +38,39 @@ class MealPlannerAdapter @Inject constructor() :
 
     inner class ViewHolder : RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SetTextI18n")
-        fun bind(item: MealPlannerEntity) {
+        fun bind(item: ResponseDetail) {
             binding.apply {
-                item.result.let { result ->
-                    //Text
-                    similarTitleTxt.text = item.result.title
-                    //Image
-                    val imageSize = result.image!!.replace(OLD_IMAGE_SIZE, NEW_IMAGE_SIZE)
-                    similarImg.load(imageSize) {
-                        crossfade(true)
-                        crossfade(800)
-                        memoryCachePolicy(CachePolicy.ENABLED)
-                        error(R.drawable.salad)
-                    }
-                    //Click
-                    root.setOnClickListener {
+                //Text
+                similarTitleTxt.text = item.title
+                //Image
+                val image = "${Constants.BASE_URL_IMAGE_RECIPES}${item.id}-${NEW_IMAGE_SIZE}"
+                similarImg.load(image) {
+                    crossfade(true)
+                    crossfade(800)
+                    memoryCachePolicy(CachePolicy.ENABLED)
+                    error(R.drawable.salad)
+                }
+                //Click
+                root.setOnClickListener {
 //                        onItemClickListener?.let {
 //                            it(item.id!!) .
-                    }
-
                 }
+
             }
         }
     }
 
 
-var onItemClickListener: ((Int) -> Unit)? = null
+    var onItemClickListener: ((Int) -> Unit)? = null
 
-fun setonItemClickListener(listener: (Int) -> Unit) {
-    onItemClickListener = listener
-}
+    fun setonItemClickListener(listener: (Int) -> Unit) {
+        onItemClickListener = listener
+    }
 
-fun setData(data: List<MealPlannerEntity>) {
-    val adapterDiffUtils = BaseDiffUtils(items, data)
-    val diffUtils = DiffUtil.calculateDiff(adapterDiffUtils)
-    items = data
-    diffUtils.dispatchUpdatesTo(this)
-}
+    fun setData(data: List<ResponseDetail>) {
+        val adapterDiffUtils = BaseDiffUtils(items, data)
+        val diffUtils = DiffUtil.calculateDiff(adapterDiffUtils)
+        items = data
+        diffUtils.dispatchUpdatesTo(this)
+    }
 }
